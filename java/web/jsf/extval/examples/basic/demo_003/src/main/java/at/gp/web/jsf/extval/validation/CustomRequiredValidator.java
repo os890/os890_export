c08d9ec2fe2d670d3b9ee26df74c60c5501b7cb6
gerhard.petracek <gerhard.petracek@68ad9bc3-5b49-0410-a736-c702f489d8d0>
2008-10-26 12:58:39 +0000

    [extval] updated example

--- java/web/jsf/extval/examples/basic/demo_003/src/main/java/at/gp/web/jsf/extval/validation/CustomRequiredValidator.java
+++ java/web/jsf/extval/examples/basic/demo_003/src/main/java/at/gp/web/jsf/extval/validation/CustomRequiredValidator.java
@@ -19,7 +19,7 @@
 package at.gp.web.jsf.extval.validation;
 
 import org.apache.myfaces.extensions.validator.core.validation.strategy.AbstractValidatorAdapter;
-import org.apache.myfaces.extensions.validator.core.annotation.AnnotationEntry;
+import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
 
 import javax.faces.context.FacesContext;
 import javax.faces.component.UIComponent;
@@ -32,7 +32,7 @@ import javax.faces.validator.ValidatorException;
 public class CustomRequiredValidator extends AbstractValidatorAdapter
 {
     protected void processValidation(FacesContext facesContext, UIComponent uiComponent,
-                                     AnnotationEntry annotationEntry, Object convertedObject) throws ValidatorException
+                                     MetaDataEntry metaDataEntry, Object convertedObject) throws ValidatorException
     {
         if("".equals(convertedObject))
         {
