535744c4fb2cf78078d656a11c707e1e28a91aa7
gerhard.petracek <gerhard.petracek@68ad9bc3-5b49-0410-a736-c702f489d8d0>
2008-11-12 16:38:46 +0000

    [extval] changed example

--- java/web/jsf/extval/examples/advanced/demo_102/src/main/java/at/gp/web/jsf/extval/validation/metadata/transformer/CustomRequiredMetaDataTransformer.java
+++ java/web/jsf/extval/examples/advanced/demo_102/src/main/java/at/gp/web/jsf/extval/validation/metadata/transformer/CustomRequiredMetaDataTransformer.java
@@ -1,17 +1,17 @@
 package at.gp.web.jsf.extval.validation.metadata.transformer;
 
 import org.apache.myfaces.extensions.validator.core.metadata.transformer.MetaDataTransformer;
+import org.apache.myfaces.extensions.validator.core.metadata.MetaDataEntry;
 
 import java.util.Map;
 import java.util.HashMap;
-import java.lang.annotation.Annotation;
 
 /**
  * @author Gerhard Petracek
  */
 public class CustomRequiredMetaDataTransformer implements MetaDataTransformer
 {
-    public Map<String, Object> convertMetaData(Annotation annotation)
+    public Map<String, Object> convertMetaData(MetaDataEntry metaDataEntry)
     {
         Map<String, Object> result = new HashMap<String, Object>();
         result.put("required", true);
