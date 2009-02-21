dbc9ed37544f19ec3628b6372c6360b157c1f444
gerhard.petracek <gerhard.petracek@68ad9bc3-5b49-0410-a736-c702f489d8d0>
2009-02-21 12:21:49 +0000

    removed client-side-validation, minor adjustments

--- java/web/jsf/extval/examples/advanced/demo_102/README.txt
+++ java/web/jsf/extval/examples/advanced/demo_102/README.txt
@@ -1,6 +1,4 @@
 illustrated features:
  - extval setup (see pom.xml)
  - custom validation + usage of the message resolver-, meta-data transformer- and component initializer mechanism
- - custom client-side validation for standard components
-   (please do >not< use the shown approach in production code.)
  - convention for package structure
--- java/web/jsf/extval/examples/advanced/demo_102/src/main/java/at/gp/web/jsf/extval/validation/StandardComponentInitializer.java
+++ java/web/jsf/extval/examples/advanced/demo_102/src/main/java/at/gp/web/jsf/extval/validation/StandardComponentInitializer.java
@@ -1,14 +1,14 @@
 package at.gp.web.jsf.extval.validation;
 
 import org.apache.myfaces.extensions.validator.core.initializer.component.ComponentInitializer;
+import org.apache.myfaces.extensions.validator.core.metadata.CommonMetaDataKeys;
+import org.apache.myfaces.extensions.validator.util.ReflectionUtils;
 
 import javax.faces.context.FacesContext;
 import javax.faces.component.UIComponent;
 import javax.faces.component.EditableValueHolder;
-import javax.faces.component.UIInput;
+import javax.faces.component.html.*;
 import java.util.Map;
-import java.util.HashMap;
-import java.lang.annotation.Annotation;
 
 /**
  * @author Gerhard Petracek
@@ -17,10 +17,59 @@ public class StandardComponentInitializer implements ComponentInitializer
 {
     public void configureComponent(FacesContext facesContext, UIComponent uiComponent, Map<String, Object> metaData)
     {
-        //just a simplified impl. - don't use it that way!
-        if(uiComponent instanceof UIInput && metaData.containsKey("required"))
+        configureRequiredAttribute(facesContext, uiComponent, metaData);
+    }
+
+    protected void configureRequiredAttribute(FacesContext facesContext,
+                                              UIComponent uiComponent,
+                                              Map<String, Object> metaData)
+    {
+        if(!processComponent(uiComponent))
         {
-            ((UIInput)uiComponent).setRequired(true);
+            return;
         }
+
+        if(Boolean.TRUE.equals(metaData.get("custom_required"))
+            &&
+            Boolean.TRUE.equals(isComponentRequired(uiComponent)))
+        {
+            ((EditableValueHolder)uiComponent).setRequired(true);
+        }
+        else if(Boolean.TRUE.equals(metaData.get(CommonMetaDataKeys.SKIP_VALIDATION)) &&
+               !Boolean.TRUE.equals(metaData.get("custom_required")))
+        {
+            ((EditableValueHolder)uiComponent).setRequired(false);
+        }
+    }
+
+    protected boolean processComponent(UIComponent uiComponent)
+    {
+        return uiComponent instanceof HtmlInputText ||
+                uiComponent instanceof HtmlInputSecret ||
+                uiComponent instanceof HtmlSelectBooleanCheckbox ||
+                uiComponent instanceof HtmlSelectOneListbox ||
+                uiComponent instanceof HtmlSelectOneMenu ||
+                uiComponent instanceof HtmlSelectOneRadio ||
+                uiComponent instanceof HtmlSelectManyCheckbox ||
+                uiComponent instanceof HtmlSelectManyListbox ||
+                uiComponent instanceof HtmlSelectManyMenu ||
+                uiComponent instanceof HtmlInputTextarea;
+    }
+
+    /**
+     * if there is no special attribute at the component which should overrule
+     * the annotated property return true!
+     *
+     * @param uiComponent component which implements the EditableValueHolder interface
+     * @return false to overrule the annotated property e.g. if component is readonly
+     */
+    protected Boolean isComponentRequired(UIComponent uiComponent)
+    {
+        boolean isReadOnly = !Boolean.FALSE.equals(ReflectionUtils.tryToInvokeMethod(
+                uiComponent, ReflectionUtils.tryToGetMethod(uiComponent.getClass(), "isReadonly")));
+        boolean isDisabled = !Boolean.FALSE.equals(ReflectionUtils.tryToInvokeMethod(
+                uiComponent, ReflectionUtils.tryToGetMethod(uiComponent.getClass(), "isDisabled")));
+
+        return !(isReadOnly || isDisabled);
     }
 }
