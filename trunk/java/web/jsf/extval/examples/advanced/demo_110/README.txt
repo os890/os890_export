d5a2d546b1395bf08f896d40f3cd3893bff27e79
gerhard.petracek <gerhard.petracek@68ad9bc3-5b49-0410-a736-c702f489d8d0>
2009-06-09 21:17:56 +0000

    demo for metadata provider add-on and severity aware validation

---
+++ trunk/java/web/jsf/extval/examples/advanced/demo_110/README.txt
@@ -0,0 +1,7 @@
+illustrated features:
+ - extval setup (see pom.xml)
+ - usage of metadata provider add-on (requires extval 1.x.2+)
+ - usage of validation parameters (in this case: ViolationSeverity.Warn.class) (requires extval x.x.3-SNAPSHOT)
+   details are available at:
+    - http://os890.blogspot.com/2009/05/preview-typesafe-constraint-aspects_31.html
+    - http://wiki.apache.org/myfaces/Extensions/Validator/ConceptOverview/Constraint_Aspects
---
+++ trunk/java/web/jsf/extval/examples/advanced/demo_110/src/main/java/at/gp/web/jsf/extval/domain/PersonMetaData.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package at.gp.web.jsf.extval.domain;
+
+import org.apache.myfaces.extensions.validator.baseval.annotation.Length;
+import org.apache.myfaces.extensions.validator.core.validation.parameter.ViolationSeverity;
+import at.gp.web.jsf.extval.validation.metadata.provider.annotation.MetaDataProvider;
+
+/**
+ * via name convention *MetaData
+ * 
+ * @author Gerhard Petracek
+ */
+@MetaDataProvider
+public class PersonMetaData
+{
+    @Length(minimum = 2, parameters = ViolationSeverity.Warn.class)
+    private String firstName;
+}
