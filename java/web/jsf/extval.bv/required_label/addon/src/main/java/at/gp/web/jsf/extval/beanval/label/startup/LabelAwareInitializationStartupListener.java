6fe4c2c2ce03c1783bf660d314b3fae93abd0c4e
gerhard.petracek <gerhard.petracek@68ad9bc3-5b49-0410-a736-c702f489d8d0>
2010-04-28 20:22:15 +0000

    first prototype of the required label add-on for the bean validation module - thx to Rudy De Busscher for some parts of the initial draft

---
+++ java/web/jsf/extval.bv/required_label/addon/src/main/java/at/gp/web/jsf/extval/beanval/label/startup/LabelAwareInitializationStartupListener.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package at.gp.web.jsf.extval.beanval.label.startup;
+
+import at.gp.web.jsf.extval.beanval.label.interceptor.BeanValidationAwareLabelRendererInterceptor;
+import at.gp.web.jsf.extval.beanval.label.interceptor.MappedConstraintSourceBeanValidationAwareLabelRendererInterceptor;
+import org.apache.myfaces.extensions.validator.beanval.BeanValidationModuleValidationInterceptor;
+import org.apache.myfaces.extensions.validator.beanval.MappedConstraintSourceBeanValidationModuleValidationInterceptor;
+import org.apache.myfaces.extensions.validator.core.ExtValContext;
+import org.apache.myfaces.extensions.validator.core.startup.AbstractStartupListener;
+
+/**
+ * @author Gerhard Petracek
+ * @author Rudy De Busscher
+ */
+public class LabelAwareInitializationStartupListener extends AbstractStartupListener
+{
+    private static final long serialVersionUID = 7048249946891862447L;
+
+    protected void init()
+    {
+        ExtValContext extValContext = ExtValContext.getContext();
+
+        deregisterDefaultImplementations(extValContext);
+        registerLabelAwareImplementations(extValContext);
+
+        activateRequiredInitializationSupport(extValContext);
+    }
+
+    private void deregisterDefaultImplementations(ExtValContext extValContext)
+    {
+        extValContext.denyRendererInterceptor(BeanValidationModuleValidationInterceptor.class);
+        extValContext.denyRendererInterceptor(MappedConstraintSourceBeanValidationModuleValidationInterceptor.class);
+    }
+
+    private void registerLabelAwareImplementations(ExtValContext extValContext)
+    {
+        extValContext.registerRendererInterceptor(new BeanValidationAwareLabelRendererInterceptor());
+        extValContext.registerRendererInterceptor(new MappedConstraintSourceBeanValidationAwareLabelRendererInterceptor());
+    }
+
+    private void activateRequiredInitializationSupport(ExtValContext extValContext)
+    {
+        extValContext.addGlobalProperty("mode:init:required", Boolean.TRUE, true);
+        // needed for some add-ons
+        // attention: you loose the compatibility with the "good old" required attribute in your pages
+        extValContext.addGlobalProperty("mode:reset:required", Boolean.TRUE, true);
+    }
+}
